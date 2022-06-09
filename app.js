9daf52c06a4aae36fba3a3674d54f096b02ea882
Mr Dominic <ochu.dominic.onah@gmail.com>
2022-06-09 11:01:11 +0100

    added left and right functionalities

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 let sliders = document.querySelectorAll(".slider");
+let leftBtn = document.querySelector(".left");
+let rightBtn = document.querySelector(".right");
 let currentPosition = 0;
 
 function activateSlider() {
@@ -10,6 +12,29 @@ function activateSlider() {
     currentPosition = 0;
   }
 }
+//the setinterval for the sliders
 setInterval(() => {
   activateSlider();
 }, 4000);
+
+// the left button
+leftBtn.addEventListener("click", () => {
+  currentPosition = currentPosition -= 1;
+  if (currentPosition < 1) {
+    currentPosition = sliders.length;
+  }
+  for (let index = 0; index < sliders.length; index++) {
+    sliders[index].style.transform = `translate(-${currentPosition - 1}00%)`;
+  }
+});
+
+// the right button of the slider
+rightBtn.addEventListener("click", () => {
+  currentPosition = currentPosition += 1;
+  if (currentPosition > sliders.length - 1) {
+    currentPosition = 0;
+  }
+  for (let index = 0; index < sliders.length; index++) {
+    sliders[index].style.transform = `translate(-${currentPosition}00%)`;
+  }
+});
